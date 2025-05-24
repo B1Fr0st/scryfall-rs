85251276e5ce84edf37bbc5fdfcb63009fba7d21
B1Fr0st <lovewolves413@gmail.com>
2025-05-24 16:38:31 -0700

    Migrate ScryfallResponse and ScryfallError to structs.rs

--- src/structs.rs
+++ src/structs.rs
@@ -4,6 +4,7 @@ use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt::Formatter;
 use std::fmt::{self, Display};
+use thiserror::Error;
 
 use url::Url;
 
@@ -22,6 +23,46 @@ pub struct OracleID(pub String);
 impl Display for OracleID {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum ScryfallResponse {
+    #[serde(rename = "card")]
+    Card(Box<Card>),
+    #[serde(rename = "error")]
+    Error(ScryfallError),
+}
+
+#[derive(Deserialize, Debug, Error)]
+pub struct ScryfallError {
+    pub status: u16,
+    pub code: String,
+    pub details: String,
+    pub type_: Option<String>,
+    pub warnings: Option<Vec<String>>,
+}
+
+impl std::fmt::Display for ScryfallError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Scryfall Error: {} - {}", self.code, self.details)
+    }
+}
+
+pub trait ToScryfallError {
+    fn to_scryfall_error(&self) -> ScryfallError;
+}
+
+impl ToScryfallError for reqwest::Error {
+    fn to_scryfall_error(&self) -> ScryfallError {
+        ScryfallError {
+            status: 500,
+            code: "reqwest_error".to_string(),
+            details: self.to_string(),
+            type_: None,
+            warnings: None,
+        }
     }
 }
 
